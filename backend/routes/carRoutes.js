df2310a20eb566833630e15221dcbef2d1d60205
mayeDave <olorunmayedavid1234@gmail.com>
2025-04-07 04:39:45 +0100

    removed auth middleware

--- backend/routes/carRoutes.js
+++ backend/routes/carRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const { createCar, getCars, getCarById, deleteCar } =
 require('../controllers/carController');
-const authMiddleware = require('../middleware/authMiddleware');
+// const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 router.get('/', getCars);
 router.get('/:id', getCarById);
-router.post('/', authMiddleware, createCar);
-router.delete('/:id', authMiddleware, deleteCar);
+router.post('/', createCar);
+router.delete('/:id', deleteCar);
 
 module.exports = router;
